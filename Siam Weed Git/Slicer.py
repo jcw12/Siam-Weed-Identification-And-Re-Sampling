3b59f1b29eadc62732ada3548cd7b6ed00e63f09
jcw12 <jcurrie987@gmail.com>
2023-06-19 17:52:45 +1000

    added reposition window
    Functionality to automatically adjust the rightmost or topmost tile to include some overlap with the previous to maintain tile size. Also added more images of different methods

--- Siam Weed Git/Slicer.py
+++ Siam Weed Git/Slicer.py
@@ -26,13 +26,19 @@ import os
 import numpy as np
 
 inputFolder = r"C:\Users\user\Documents\GitHub\Siam-Weed-Identification-And-Re-Sampling\Siam Weed Git\UnSliced"
-outputFolder = r"C:\Users\user\Documents\GitHub\Siam-Weed-Identification-And-Re-Sampling\Siam Weed Git\Aggregated"
+outputFolder = r"C:\Users\user\Documents\GitHub\Siam-Weed-Identification-And-Re-Sampling\Siam Weed Git\Test"
 
 #Set downsampling method and downsampling factor here:
 Downsampling = True
-#Options for method include INTER_CUBIC, INTER_LANCZOS4, INTER_LINEAR
+
+"""
+Need to make it all one function, where the downsampling method can be chosen and the factor can be chosen,
+with a factor of 1 as an option. Also need to adjust the ends so that the moving window doesn't clip the ends.'
+"""
+
+#Options for method include cv2.INTER_CUBIC, cv2.INTER_LANCZOS4, cv2.INTER_LINEAR, PIXEL_AGG. PIXEL_AGG is the recommended method for most realistic upscaled images.
 Method = cv2.INTER_LANCZOS4
-Factor = 2
+Factor = 8
 
 #Tile Sizes:
 tile_size = (456, 456)
@@ -41,10 +47,29 @@ offset = (456, 456) # the size of image must be divisible by the offset value. O
 def sliceEmAll(imageName, img):
     #img = cv2.imread(imageName)
     img_shape = img.shape
+    print(img_shape)
+    offset_fit_0 = img_shape[0] % offset[0]
+    offset_fit_1 = img_shape[1] % offset[1]
     #This function slices an image into m*n tiles. The code is adopted from https://stackoverflow.com/questions/45950124/creating-image-tiles-mn-of-original-image-using-python-and-numpy
     for i in range(int(math.ceil(img_shape[0]/(offset[1] * 1.0)))):
+        if offset_fit_0 != 0 and i == math.ceil(img_shape[0]/(offset[1] * 1.0)):
+            window_top = offset[1]*i-offset_fit_0
         for j in range(int(math.ceil(img_shape[1]/(offset[0] * 1.0)))):
-            cropped_img = img[offset[1]*i:min(offset[1]*i+tile_size[1], img_shape[0]), offset[0]*j:min(offset[0]*j+tile_size[0], img_shape[1])]
+            print(offset*j)
+            print(math.ceil(img_shape[1]/(offset[1] * 1.0)))
+            #If offsets need to be made and it's the last window
+            if offset_fit_1 != 0 and j == math.ceil(img_shape[1]/(offset[0] * 1.0))-1:
+                print('Adjust this!', i,j)
+                window_top = offset[1]*i
+                window_height = min(offset[1]*i+tile_size[1], img_shape[0])
+                window_start = offset[0]*j - offset_fit_1
+                window_length = min(offset[0]*j+tile_size[0], img_shape[1])
+            else:
+                window_top = offset[1]*i
+                window_height = min(offset[1]*i+tile_size[1], img_shape[0])
+                window_start = offset[0]*j
+                window_length = min(offset[0]*j+tile_size[0], img_shape[1])
+            cropped_img = img[window_top:window_height, window_start:window_length]
             # Debugging the tiles
             #print(img)
             print(imageName)
@@ -81,53 +106,42 @@ def downsample(img):
     #average proportional to the area of the input etc.
     # Define aggregation factor
     data = cv2.imread(img)
+    
+    print(data.shape)
     aggregation_factor = Factor
     print(data[0:8,0:8,1])
+    if Method == 'PIXEL_AGG' and Downsampling == True:
+        print('Downsampling using', Method, 'and a scale factor of',Factor)
+        # Compute the padding required
+        padding_rows = data.shape[0] % aggregation_factor
+        padding_cols = data.shape[1] % aggregation_factor
+        
+        # Pad the array with zeros
+        padded_data = np.pad(data, ((0, padding_rows), (0, padding_cols), (0, 0)), mode='constant')
+        print(padded_data.shape)
+        
+        # Reshape the padded ndarray
+        reshaped_data = padded_data.reshape(padded_data.shape[0] // aggregation_factor, aggregation_factor,
+                                        padded_data.shape[1] // aggregation_factor, aggregation_factor, data.shape[2])
+        
+        # Compute the mean along the specified axes
+        aggregated_data = np.mean(reshaped_data, axis=(1, 3))
+        sliceEmAll(img,aggregated_data)
+    elif Downsampling == True:
+        print('Downsampling using', Method, 'and a scale factor of',Factor)
+        #https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
+        #https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#resize
+        width = int(data.shape[1] / Factor)
+        height = int(data.shape[0] / Factor)
+        shape = (width, height)
+        data = cv2.resize(data, shape, interpolation = Method)
+        sliceEmAll(img, data)
+    else:
+        print('Not Downsampling.')
 
-    # Compute the padding required
-    padding_rows = data.shape[0] % aggregation_factor
-    padding_cols = data.shape[1] % aggregation_factor
-
-    # Pad the array with zeros
-    padded_data = np.pad(data, ((0, padding_rows), (0, padding_cols), (0, 0)), mode='constant')
-    print(padded_data.shape)
-
-    # Reshape the padded ndarray
-    reshaped_data = padded_data.reshape(padded_data.shape[0] // aggregation_factor, aggregation_factor,
-                                    padded_data.shape[1] // aggregation_factor, aggregation_factor, data.shape[2])
-
-    # Compute the mean along the specified axes
-    aggregated_data = np.mean(reshaped_data, axis=(1, 3))
-    print(aggregated_data[0,0],aggregated_data[0,1])
-    print("""aggregated:
-""",aggregated_data[0:8,0:8,1])
-    
-    #sliceEmAll(img, aggregated_data)
-
-
-"""
-def downsample(image):
-    #https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
-    #https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#resize
-    img = cv2.imread(image)
-    width = int(img.shape[1] / Factor)
-    height = int(img.shape[0] / Factor)
-    shape = (width, height)
-    img = cv2.resize(img, shape, interpolation = Method)
-    sliceEmAll(image, img)
-
-
-    
-"""
-    
-    
-    
-    
     
 inputImages = load_images_from_folder(inputFolder)
-    
-    
-    
+print('Complete')
     
 # downsampling
 # original 2 cm
@@ -135,6 +149,7 @@ inputImages = load_images_from_folder(inputFolder)
 # output 8 cm pixel 
 
 #dipsgautam@gmail.com
+#jcurrie987@gmail.com
        
        
 
